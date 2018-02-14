7048fbec52a3db36d8f2ae82b450c9c1a753d12e
bru1987 <bru1987@gmail.com>
2018-02-14 11:53:01 -0200

    Criando as primeiras versões do curso
    Descrição

---
+++ executando-arquivo-python-jupyter.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+print ("Se você está vendo esse texto, é porque o comando foi executado com sucesso! Isso porque o arquivo contém somente um printf com esta string dentro!")
