d509ed1cf20a85e877cd2b9d41bf48ae08f42ac9
bru1987 <bru1987@gmail.com>
2018-02-16 16:42:32 -0200

    s
    a

--- Introdução a Python/helloworld-top.py
+++ Introdução a Python/helloworld-top.py
@@ -5,7 +5,9 @@ import sys
 
 # Todo o código da função main()
 def main():
-    print ('Hello there', sys.argv[1])
+    print ("Olá", sys.argv[1]+"!")
+    #Note que a vírgula faz com que apareça um caractere em branco
+    
     # Os argumentos da linha de comando estão em sys.argv[1], sys.argv[2] ...
     # sys.argv[0] é o nome do script, pode ser ignorado
 
